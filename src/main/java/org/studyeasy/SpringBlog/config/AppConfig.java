6510cacf170cbb1a8efb096c0dc2c129674e5ba5
Rohan07github <rohan071209@gmail.com>
2025-08-07 23:05:21 +0530

    update in appconfig.java file

--- src/main/java/org/studyeasy/SpringBlog/config/AppConfig.java
+++ src/main/java/org/studyeasy/SpringBlog/config/AppConfig.java
@@ -11,13 +11,50 @@ import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 @Configuration
 public class AppConfig {
+
+    @Value("${spring.mail.properties.mail.transport.protocol}")
+    private String mail_transport_protocol;
+
+    @Value("${spring.mail.port}")
+    private String spring_mail_port;
+
+    @Value("${spring.mail.username}")
+    private String spring_mail_username;
+
+    @Value("${spring.mail.password}")
+    private String spring_mail_password;
+
+    @Value("${mail.smtp.auth}")
+    private boolean mail_smtp_Auth;
+
+    @Value("${spring.mail.properties.mail.smtp.starttls.enable}")
+    private boolean mail_smtp_starttls_enable;
+
+    @Value("${spring.mail.ssl.trust}")
+    private String smtp_ssl_Trust;
     
+    @Value("${spring.mail.host}")
+    private String spring_mail_host;
+
     @Bean
-    public JavaMailSender getJavaMailSender(JavaMailSenderImpl mailSender) {
-        // Spring Boot already configures most settings automatically
-        // You can just add any additional properties here if needed
+    public JavaMailSender getJavaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+    
+        mailSender.setHost(spring_mail_host);
+        mailSender.setPort(Integer.parseInt(spring_mail_port));
+
+        mailSender.setUsername(spring_mail_username);
+        mailSender.setPassword(spring_mail_password);
+
         Properties props = mailSender.getJavaMailProperties();
-        props.put("mail.debug", "true");
+        props.put("mail.transport.protocol", mail_transport_protocol);
+        props.put("mail.smtp.auth", mail_smtp_Auth);
+        props.put("mail.smtp.starttls.enable", mail_smtp_starttls_enable);
+        props.put("mail.smtp.ssl.trust", smtp_ssl_Trust);
+        props.put("mail.debug", "true");    
+
         return mailSender;
+
+
     }
- }
+}
